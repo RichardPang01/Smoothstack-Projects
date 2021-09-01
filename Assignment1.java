b43406403b46959bd0222a9c0649a0e55001876f
RichardPang01 <89930330+RichardPang01@users.noreply.github.com>
2021-09-01 12:50:16 -0400

    Add files via upload

---
+++ Assignment1.java
@@ -0,0 +1,8 @@
+public class Assignment1{
+   public static void main(String [] args){
+      System.out.println("1)\n*\n**\n***\n****\n.........");
+      System.out.println("2)\n.........\n****\n***\n**\n*");
+      System.out.println("3)\n     *\n    ***\n   *****\n  *******\n...........");
+      System.out.println("4)\n...........\n  *******\n   *****\n    ***\n     *");
+   }
+}
